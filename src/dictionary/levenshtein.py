b43f6ab27acc4de2cb01437bafaf6ea6c0c3e874
Andrew Kurauchi <andrew.kurauchi@gmail.com>
2015-01-20 13:13:08 -0500

    Rows in levenshtein distance function now represent fixations instead of words (so it will be easier to use previous results)

--- src/dictionary/levenshtein.py
+++ src/dictionary/levenshtein.py
@@ -7,46 +7,43 @@ def levenshtein(word, fixations):
 
     # create two work vectors of integer distances
     # initialize v0 (the previous row of distances)
-    # this row is A[0][i]: edit distance for an empty fixation set
-    # the distance is just the number of characters to delete from word
-    v0 = [i for i in range(len(word))]
-    v0.append(len(word))
-    v1 = [0]*(len(word)+1)
-    last_dist = v0[-1]
-    used = [] # list of fixations used in this matching
+    # this row is A[0][i]: edit distance for an empty word
+    # the distance is always zero as the deletion of a fixation has cost 0
+    # (IMPORTANT: this is different from the lenveshtein distance!)
+    v0 = [0]*(len(fixations)+1)
+    v1 = [0]*(len(fixations)+1)
+    last_dist = 0
+    used_word = ''
 
-    for i in range(len(fixations)):
+    for i in range(len(word)):
         # calculate v1 (current row distances) from the previous row v0
 
         # first element of v1 is A[i+1][0]
-        #   edit distance is delete (i+1) fixations to match empty word
-        #   cost of deleting fixations is 0
-        v1[0] = 0
+        #   edit distance is delete (i+1) chars in word to match empty fixation list
+        #   cost of deleting char in word is 1
+        v1[0] = v0[0] + 1
 
         # use formula to fill in the rest of the row
-        for j in range(len(word)):
-            cost = 0 if word[j] in fixations[i] else 1
+        for j in range(len(fixations)):
+            cost = 0 if word[i] in fixations[j] else 1
             # IMPORTANT: the following line is different from the levenshtein
             # distance. We don't penalize deletions from fixations list.
-            v1[j + 1] = min([v1[j] + 1, v0[j + 1], v0[j] + cost])
-        if v1[-1] < last_dist:
-            last_dist = v1[-1]
-            used.append(i)
+            v1[j + 1] = min([v1[j], v0[j + 1] + 1, v0[j] + cost])
+
+        if last_dist == v1[-1]:
+            used_word += word[i]
+        last_dist = v1[-1]
 
         # copy v1 (current row) to v0 (previous row) for next iteration
-        for j in range(len(word)):
+        for j in range(len(fixations) + 1):
             v0[j] = v1[j]
 
-    used_word = ''
-    for i in range(len(word)):
-        if v1[i] == v1[i+1]:
-            used_word += word[i]
-    return v1[len(word)], used_word, used
+    used = [i for i in range(len(fixations)) if v1[i+1] < v1[i]] # list of fixations used in this matching
+    return v1[len(fixations)], used_word, used
 
 if __name__=='__main__':
     import os
     from predict import load_char_sets
-
 
     vid = '../../videos'
     sbjs = [os.path.join(vid,s) for s in os.listdir(vid)]
